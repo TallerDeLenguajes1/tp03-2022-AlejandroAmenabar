92a2f0f25ff8d81a9ff9d3f80d90d14383024e40
Alejandro Amenabar <alegymnas@gmail.com>
2022-04-07 19:31:32 -0300

    segundo commit

---
+++ tp3_4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    int cant;
+    char *buff;
+    char **V;
+    printf("Ingrese la cantidad de nombres a ingresar:");
+    scanf("%i",&cant);
+    getchar();
+    V = (char**)malloc(sizeof(char *) * cant);
+
+    buff = (char *)malloc(100 * sizeof(char));
+    
+    for (int i=0; i<cant; i++)
+    {
+        printf("Ingrese el %d) nombre: ",i);
+        gets(buff);
+        *(V+i)=(char *)malloc((strlen(buff) + 1) * sizeof(char));
+        strcpy(*(V+i),buff);
+    }
+    
+    for (int j=0; j<cant; j++)
+    {
+        printf("\n los nombres ingresados son: %s",*(V+j));
+    }
+        
+    for (int i=0; i<cant; i++)
+    {
+        free(V[i]);
+    }
+    
+    free(buff);
+    return 0;
+}
